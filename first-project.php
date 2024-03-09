dc5aa9a704134ffcd362bf594bcfef75f087a04d
Hami Ibriyamov <hamop3108@gmail.com>
2024-03-09 17:15:35 +0200

    Create first-project.php

---
+++ first-project.php
@@ -0,0 +1,21 @@
+
+
+
+<?php
+echo "php rocks";
+echo "<br>";
+echo "or does it?";
+?>
+
+<!Doctype html>
+<html>
+	<head>
+		<title><?php echo 'PHP Loaded Page Title'; ?> </title>
+	</head>
+	<body>
+		<h1><?php echo "My first PHP project!";?></h1>
+		<p><?php echo "This paragraph was created using PHP and HTML!"; ?></p>
+	</body>
+</html>
+
+<?php echo "<h2> This heading was entirely created with PHP! </h2>";
