c20009e903742fb9d322c891048597979cf9a0a8
Hami Ibriyamov <hamop3108@gmail.com>
2024-03-09 20:25:43 +0200

    Update first-project.php

--- first-project.php
+++ first-project.php
@@ -10,6 +10,12 @@ $h_2 = "<h2> This heading was entirely created with PHP! </h2>";
 
 define("CONSTANT", "This is a constant", false); //define(Name of const, const content, IsCaseSensitive - by default false);
 define("NUMBER", 99, /*true*/); // case sensitive is not supported anymore!
+
+$isThisHard = false;
+
+$var = null;
+$var = "Not null anymore";
+
 ?>
 
 <!Doctype html>
@@ -22,7 +28,14 @@ define("NUMBER", 99, /*true*/); // case sensitive is not supported anymore!
 		<p><?php echo $p; ?></p>
 		<div><?php echo $h_2?></div>
 		<div id=constants>
-			<?php echo CONSTANT; echo "<br>" ;echo NUMBER; ?>
+			<?php echo CONSTANT; 
+			echo "<br>" ;
+			echo NUMBER;
+			echo "<br>";
+			var_dump($isThisHard);
+			echo "<br>";
+			var_dump($var);
+			?>
 		</div>
 	</body>
 </html>
