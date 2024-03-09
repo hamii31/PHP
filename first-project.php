96de0520792cb9b5f537c175ff39d2c2e58dd70f
Hami Ibriyamov <hamop3108@gmail.com>
2024-03-09 20:12:07 +0200

    Update first-project.php

--- first-project.php
+++ first-project.php
@@ -7,6 +7,9 @@ $h =  "My first PHP project!";
 $p = "This paragraph was created using PHP and HTML!";
 
 $h_2 = "<h2> This heading was entirely created with PHP! </h2>";
+
+define("CONSTANT", "This is a constant", false); //define(Name of const, const content, IsCaseSensitive - by default false);
+define("NUMBER", 99, /*true*/); // case sensitive is not supported anymore!
 ?>
 
 <!Doctype html>
@@ -18,5 +21,8 @@ $h_2 = "<h2> This heading was entirely created with PHP! </h2>";
 		<h1><?php echo $h; ?></h1>
 		<p><?php echo $p; ?></p>
 		<div><?php echo $h_2?></div>
+		<div id=constants>
+			<?php echo CONSTANT; echo "<br>" ;echo NUMBER; ?>
+		</div>
 	</body>
 </html>
